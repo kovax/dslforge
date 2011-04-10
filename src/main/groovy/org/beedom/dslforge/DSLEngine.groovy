b4475440d243a55c1ef070871903f1e89eea0589
Zsolt Kovacs <zs.myth@gmail.com>
2011-04-10 17:56:17 +0200

    minor change + idea module

--- src/main/groovy/org/beedom/dslforge/DSLEngine.groovy
+++ src/main/groovy/org/beedom/dslforge/DSLEngine.groovy
@@ -224,11 +224,11 @@ public class DSLEngine  {
 
             log.info( "Try to enhance the script with the dslKey: '$dslKey'" )
 
-            //TODO: modify original Script object using GroovyScriptEngine and GroovyClassLoader.parse()
-            String t = new File(scriptsHome+"/"+scriptName).text
+            //TODO: modify original Script object using GroovyScriptEngine and GroovyClassLoader.parse() (how???)
+            String scriptText = new File(scriptsHome+"/"+scriptName).text
 
             //wraps the script with a Closure containing the default dslKey
-            return run( new GroovyShell().evaluate( "{->${dslKey} {${t}}}" ) )
+            return run( new GroovyShell().evaluate( "{->${dslKey} {${scriptText}}}" ) )
         }
         else {
             //last minute initialisation of GroovyScriptEngine
